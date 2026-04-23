3c4380f04a181c3dc0e3b869da7f053cc64a382c
apvcode <wldr334@gmail.com>
2026-04-24 02:00:10 +0300

    app icon

--- TMessagesProj/src/main/java/space/axolab/axogram/BuildVars.java
+++ TMessagesProj/src/main/java/space/axolab/axogram/BuildVars.java
@@ -30,7 +30,7 @@ public class BuildVars {
     public static String APP_HASH = "";
 
     // SafetyNet key for Google Identity SDK, set it to empty to disable
-    public static String SAFETYNET_KEY = "AIzaSyDqt8P-7F7CPCseMkOiVRgb1LY8RN1bvH8";
+    public static String SAFETYNET_KEY = "";
     public static String PLAYSTORE_APP_URL = "https://play.google.com/store/apps/details?id=space.axolab.axogram";
     public static String HUAWEI_STORE_URL = "https://appgallery.huawei.com/app/C101184875";
     public static String GOOGLE_AUTH_CLIENT_ID = "760348033671-81kmi3pi84p11ub8hp9a1funsv0rn2p9.apps.googleusercontent.com";
